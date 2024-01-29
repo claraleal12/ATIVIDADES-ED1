5510346b36d7e558d283f6e26f280dc7f38c655a
clara leal <147611128+claraleal12@users.noreply.github.com>
2024-01-28 21:42:34 -0300

    Add files via upload

---
+++ teste1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (void){
+    int x, y; 
+    y = 2; 
+    int *p1, *p2; 
+    p1 = &y; 
+    x = 1;
+    p2 = p1; 
+    (*p1)++; 
+    printf("%d %d", x, y); 
+
+return 0;
+}
